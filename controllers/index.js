19e6ead2f855cab09786e44dfd09083e148e79a5
Wayne <22385094+wayne989@users.noreply.github.com>
2018-06-12 08:44:16 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,6 @@ var express = require('express');
 var router = express.Router();
 let interceptLogger = require('../middlewares/InterceptLogger');
 
-router.use('/', interceptLogger, require('./Token'));
 router.use('/', interceptLogger, require('./ReadFile'));
-router.use('/', interceptLogger, require('./Identity'));
 router.use('/', interceptLogger, require('./MockService'));
 module.exports = router;
